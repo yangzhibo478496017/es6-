7f7f9dd3fdb06b990434db7b01a033f8407bfd81
yangzhibo478496017 <15102517035@163.com>
2020-06-05 00:44:31 +0800

    test

--- test.js
+++ test.js
@@ -897,7 +897,7 @@ const {log} = console;
   // 报错
 // fetch('http://example.com',{body:1,method: 2,headers:3})
 
-//默认值  是只有等式左边为null时  才等于右边
+//默认值  是只有等式左边为undefined时  才等于右边  ，null则没有这个效果。
 // 写法一
 // function m1({x = 0, y = 0} = {}) {
 //     log(  [x, y])
@@ -928,6 +928,194 @@ const {log} = console;
 // m1({z: 3}) // [0, 0]
 // m2({z: 3}) // [undefined, undefined]
 
+// 函数的 length 属性
+// 指定了默认值以后，函数的length属性，将返回没有指定默认值的参数个数。也就是说，指定了默认值后，length属性将失真。
 
-//test1
+// (function (a) {}).length // 1
+// (function (a = 5) {}).length // 0
+// (function (a, b, c = 5) {}).length // 2
+// 上面代码中，length属性的返回值，等于函数的参数个数减去指定了默认值的参数个数。比如，上面最后一个函数，定义了 3 个参数，其中有一个参数c指定了默认值，因此length属性等于3减去1，最后得到2。
 
+// 这是因为length属性的含义是，该函数预期传入的参数个数。某个参数指定默认值以后，预期传入的参数个数就不包括这个参数了。同理，后文的 rest 参数也不会计入length属性。
+
+// (function(...args) {}).length // 0
+// 如果设置了默认值的参数不是尾参数，那么length属性也不再计入后面的参数了。
+
+// (function (a = 0, b, c) {}).length // 0
+// (function (a, b = 1, c) {}).length // 1
+
+
+// 作用域
+// 一旦设置了参数的默认值，函数进行声明初始化时，参数会形成一个单独的作用域（context）。等到初始化结束，这个作用域就会消失。这种语法行为，在不设置参数默认值时，是不会出现的。
+
+// let x = 1;
+// var x = 1;
+// function f(x, y = x) {
+//   console.log(y);
+// }
+// var y = 3;
+// f(2) // 2
+
+// 上面代码中，参数y的默认值等于变量x。调用函数f时，参数形成一个单独的作用域。在这个作用域里面，默认值变量x指向第一个参数x，而不是全局变量x，所以输出是2。
+
+
+// let x = 1;
+
+// function f(y = x) {
+//   let x = 2;
+//   console.log(y);
+// }
+
+// f() // 1
+// 下面这样写，也会报错。
+
+// var x = 1;
+
+// function foo(x = x) {   //let x = x  
+//   // ...
+// }
+
+// foo() // ReferenceError: x is not defined
+// 上面代码中，参数x = x形成一个单独作用域。实际执行的是let x = x，由于暂时性死区的原因，这行代码会报错”x 未定义“。
+
+// let foo = 'outer';
+
+// function bar(func = () => foo) {
+//   let foo = 'inner';
+//   console.log(func());
+// }
+
+// bar(); 
+
+
+
+// var x = 1;
+// function foo(x, y = function() { x = 2; }) {  //let x  , let y = f
+//    var  x = 3;
+//   y();
+//   console.log(x);
+// }
+
+// foo() // 3
+// log(x) // 1
+
+// 上面代码中，函数foo的参数形成一个单独作用域。这个作用域里面，首先声明了变量x，然后声明了变量y，y的默认值是一个匿名函数。
+// 这个匿名函数内部的变量x，指向同一个作用域的第一个参数x。函数foo内部又声明了一个内部变量x，该变量与第一个参数x由于不是同一个作用域，所以不是同一个变量，因此执行y后，内部变量x和外部全局变量x的值都没变。
+
+
+// 应用
+// 利用参数默认值，可以指定某一个参数不得省略，如果省略就抛出一个错误。
+
+// function throwIfMissing() {
+//   throw new Error('Missing parameter');
+// }
+
+// function foo(mustBeProvided = throwIfMissing()) {
+//   return mustBeProvided;
+// }
+
+// foo()
+// Error: Missing parameter
+// 上面代码的foo函数，如果调用的时候没有参数，就会调用默认值throwIfMissing函数，从而抛出一个错误。
+
+// 从上面代码还可以看到，参数mustBeProvided的默认值等于throwIfMissing函数的运行结果（注意函数名throwIfMissing之后有一对圆括号），这表明参数的默认值不是在定义时执行，而是在运行时执行。如果参数已经赋值，默认值中的函数就不会运行。
+
+// 另外，可以将参数默认值设为undefined，表明这个参数是可以省略的。
+
+// function foo(optional = undefined) { ··· }
+
+// rest 参数
+// ES6 引入 rest 参数（形式为...变量名），用于获取函数的多余参数，这样就不需要使用arguments对象了。rest 参数搭配的变量是一个数组，该变量将多余的参数放入数组中。
+
+// function add(...values) {
+//   let sum = 0;
+
+//   for (var val of values) {
+//     sum += val;
+//   }
+
+//   return sum;
+// }
+
+// log(add(2, 5, 3,4,5,6,7,8,9)) // 10
+// 上面代码的add函数是一个求和函数，利用 rest 参数，可以向该函数传入任意数目的参数。
+
+// 下面是一个 rest 参数代替arguments变量的例子。
+
+// arguments变量的写法
+// function sortNumbers() {
+//   return Array.prototype.slice.call(arguments).sort();
+// }
+
+// // rest参数的写法
+// const sortNumbers1 = (...numbers) => numbers.sort();
+
+// log(sortNumbers(1,2,3,7,8,9,4,5,6))
+// log(sortNumbers1(7,8,1,2,3,4,5,6,9))
+
+
+
+// function push(array, ...items) {
+//   items.forEach(function(item) {
+//     array.push(item);
+//     console.log(item);
+//   });
+// }
+
+// function push1() {
+//   let a = Array.prototype.slice.call(arguments)
+//   let b = a[0];
+//   for (let index = 1; index < a.length; index++) {
+//     let element = a[index];
+//     b.push(element);
+//   }
+//   log(b);
+// }
+
+
+
+// var a = [];
+// push(a, 1, 2, 3)
+// push1(a, 1, 2, 3)
+
+
+// rest 参数之后不能再有其他参数（即只能是最后一个参数），否则会报错。
+
+// 报错
+// function f(a, ...b, c) {
+//   // ...
+// }
+// 函数的length属性，不包括 rest 参数。
+
+// (function(a) {}).length  // 1
+// (function(...a) {}).length  // 0
+// (function(a, ...b) {}).length  // 1
+
+
+// ES2016 做了一点修改，规定只要函数参数使用了默认值、解构赋值、或者扩展运算符，那么函数内部就不能显式设定为严格模式，否则会报错。
+
+// 报错
+// function doSomething(a, b = a) {
+//   'use strict';
+//   // code
+// }
+
+// // 报错
+// const doSomething = function ({a, b}) {
+//   'use strict';
+//   // code
+// };
+
+// // 报错
+// const doSomething = (...a) => {
+//   'use strict';
+//   // code
+// };
+
+// const obj = {
+//   // 报错
+//   doSomething({a, b}) {
+//     'use strict';
+//     // code
+//   }
+// };
